58428efd190960c6d59eb22807dc05e317b23a79
Jude <sungkisa@naver.com>
2021-08-01 20:09:55 +0900

    feat: MinHeap으로 다익스트라를 만들자

--- src/algorithms/dijkstra.ts
+++ src/algorithms/dijkstra.ts
@@ -4,7 +4,6 @@ import { Heap, HeapPayload } from './heap';
 
 export function dijkstra(start: number, graph: Graph) {
   const d: number[] = [];
-  const visited: boolean[] = [];
   const heap: Heap = new Heap();
 
   for (let i = 1; i < graph.getMaxNode(); i++) {
@@ -24,7 +23,6 @@ export function dijkstra(start: number, graph: Graph) {
         });
       }
     }
-    visited[currentNode] = true;
 
     const shortest = heap.remove();
     if (shortest == false) break;
@@ -33,25 +31,4 @@ export function dijkstra(start: number, graph: Graph) {
   }
 
   console.log(d);
-}
-
-function findShortestUnvisitedNode(
-  d: number[],
-  start: number,
-  visited: boolean[]
-): boolean | number {
-  const unvisited = d
-    .map((n, i) => ({ index: i, n: n }))
-    .filter((v, i) => !visited[i] && i != start);
-
-  if (unvisited.length == 0) return false;
-
-  const sorted = unvisited.sort((a, b) => {
-    if (a.n > b.n) return 1;
-    else if (a.n < b.n) return -1;
-    else return 0;
-  });
-
-  console.log(sorted);
-  return sorted[0].index;
 }
